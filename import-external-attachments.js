e7784b6b3c570b5156c41688c647e615521da2a7
Ryan P.C. McQuen <ryan.q@linux.com>
2016-02-15 15:00:36 -0800

    cleaning up more javascript
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- import-external-attachments.js
+++ import-external-attachments.js
@@ -27,6 +27,10 @@
     );
   };
 
+  var d = new Date();
+  var import_images_start_time;
+  import_images_start_time = d.getTime();
+
   /**
    * recursive function for resizing images
    */
@@ -43,27 +47,26 @@
 
     var target = jQuery('#import_process');
 
-    var d = new Date();
     var current_time = d.getTime();
 
-    if (current_time > (window.import_images_start_time + 60000) && window.import_images_one_minute !== 'set') {
+    if (current_time > (import_images_start_time + 60000) && window.import_images_one_minute !== 'set') {
       target_message.prepend('<div>1 minute passed... Looking good!</div>');
       window.import_images_one_minute = 'set';
     }
-    if (current_time > (window.import_images_start_time + 180000) && window.import_images_three_minute !== 'set') {
+    if (current_time > (import_images_start_time + 180000) && window.import_images_three_minute !== 'set') {
       target_message.prepend('<div>3 minutes passed... Still looking good!</div>');
       window.import_images_three_minute = 'set';
     }
-    if (current_time > (window.import_images_start_time + 300000) && window.import_images_five_minute !== 'set') {
+    if (current_time > (import_images_start_time + 300000) && window.import_images_five_minute !== 'set') {
       target_message.prepend('<div>5 minutes have passed... Still looking good... But if we seem stuck on one post, you may want to refresh and try again.</div>');
       window.import_images_five_minute = 'set';
     }
-    if (current_time > (window.import_images_start_time + 600000) && window.import_images_ten_minute !== 'set') {
+    if (current_time > (import_images_start_time + 600000) && window.import_images_ten_minute !== 'set') {
       target_message.prepend('<div>10 minutes have passed... Woah! You have lot of images.</div>');
       window.import_images_ten_minute = 'set';
       var lets_stop_here = true;
     }
-    if (current_time > (window.import_images_start_time + 1200000) && window.import_images_twenty_minute !== 'set') {
+    if (current_time > (import_images_start_time + 1200000) && window.import_images_twenty_minute !== 'set') {
       target_message.prepend("<div style='color:#990000'>20 minutes have passed... You REALLY have lot of images... Let's take a break. Refresh this page to start again.</div>");
       window.import_images_twenty_minute = 'set';
     }
@@ -75,7 +78,6 @@
     if (window.import_images_twenty_minute !== 'set') {
 
       jQuery.post(ajaxurl, data, function (response) {
-        //alert(window.import_images_start_time + ' -- ' + current_time);
         var result = JSON.parse(response);
 
         target.prepend('<div style="padding:2px 30px 0 10px;">' + result + '</div>');
@@ -90,48 +92,42 @@
   };
 
 
-  var import_images_start_timer = function () {
 
-    var d = new Date();
-    window.import_images_start_time = d.getTime();
-  };
-
-
-  var external_images_import_images = function () {
-
-    import_images_start_timer();
-    window.import_images_one_minute = '';
-    window.import_images_three_minute = '';
-    window.import_images_five_minute = '';
-    window.import_images_ten_minute = '';
-
-    var target = jQuery('#import_posts');
-    jQuery('#posts_list').fadeOut('3000');
-
-    target.html('');
-    target.show();
-    //jQuery(document).scrollTop(target.offset().top);
-
-    var data = {
-      action: 'external_image_get_backcatalog_ajax'
-    };
-
-    jQuery.post(ajaxurl, data, function (response) {
-      //alert('Got this from the server: ' + response);
-
-      var results = JSON.parse(response);
-      var posts_to_process = results.posts;
-      var insert = '<div id="processing"><p class="howto">Importing images from ' + posts_to_process.length + ' posts...</p><h2>Please Wait...</h2></div>';
-      insert += '<div id="import_process" style="padding:2px 2px 10px; margin:10px 0;background: none repeat scroll 0 0 #C5E5F5;border: 1px solid #298CBA;"></div>';
-      target.html(insert);
-
-      target.slideDown();
-      // recurse
-      external_images_import_all(posts_to_process, 0);
-    });
-  };
+  //
+  // var external_images_import_images = function () {
+  //
+  //   import_images_start_time();
+  //   window.import_images_one_minute = '';
+  //   window.import_images_three_minute = '';
+  //   window.import_images_five_minute = '';
+  //   window.import_images_ten_minute = '';
+  //
+  //   var target = jQuery('#import_posts');
+  //   jQuery('#posts_list').fadeOut('3000');
+  //
+  //   target.html('');
+  //   target.show();
+  //   //jQuery(document).scrollTop(target.offset().top);
+  //
+  //   var data = {
+  //     action: 'external_image_get_backcatalog_ajax'
+  //   };
+  //
+  //   jQuery.post(ajaxurl, data, function (response) {
+  //     //alert('Got this from the server: ' + response);
+  //
+  //     var results = JSON.parse(response);
+  //     var posts_to_process = results.posts;
+  //     var insert = '<div id="processing"><p class="howto">Importing images from ' + posts_to_process.length + ' posts...</p><h2>Please Wait...</h2></div>';
+  //     insert += '<div id="import_process" style="padding:2px 2px 10px; margin:10px 0;background: none repeat scroll 0 0 #C5E5F5;border: 1px solid #298CBA;"></div>';
+  //     target.html(insert);
+  //
+  //     target.slideDown();
+  //     // recurse
+  //     external_images_import_all(posts_to_process, 0);
+  //   });
+  // };
 
   window.external_images_import_all = external_images_import_all;
-  window.import_images_start_timer = import_images_start_timer;
 
 }());
