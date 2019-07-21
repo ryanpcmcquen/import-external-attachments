8b529cb994dbdab7b40755f95865deecf55cdc1b
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-20 22:27:06 -0700

    Remove license header.

--- import-external-attachments.php
+++ import-external-attachments.php
@@ -16,20 +16,6 @@ https://github.com/MartyThornley/import-external-images
 
 based on Add Linked Images To Gallery v1.4 by Randy Hunt
 http://www.bbqiguana.com/wordpress-plugins/add-linked-images-to-gallery/
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 	$external_image_count = 0;
